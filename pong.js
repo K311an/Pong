d6746d7982bd1938f63442244be41e6841e76b31
K311an <150956248+K311an@users.noreply.github.com>
2025-08-22 12:39:11 -0600

    Create pong.js

---
+++ pong.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Simple Pong</title>
+  <style>
+    canvas {
+      background: #000;
+      display: block;
+      margin: 0 auto;
+    }
+  </style>
+</head>
+<body>
+  <canvas id="pong" width="800" height="400"></canvas>
+  <script src="pong.js"></script>
+</body>
+</html>
